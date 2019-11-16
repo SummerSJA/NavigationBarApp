a0214161f3c800616c8703b5db7daff1d5b40dfa
Summer Abdulaal <Summer@electricred.com>
2019-11-15 19:08:51 -0500

    Initial Commit

---
+++ NavigationBarApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationBarApp
+//
+//  Created by Summer Abdulaal on 11/15/19.
+//  Copyright © 2019 Summer Abdulaal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
